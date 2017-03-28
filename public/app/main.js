bcaea87f0703830474ef437e44e04e23d963b2e2
Sander De la Marche <sanderdlm69@gmail.com>
2017-03-28 18:19:38 +0200

    Added SVG overlay for dots, added click-to-create paths between dots, updated flexbox layout, added percentage counter, updated footer links, etc..

--- public/app/main.js
+++ public/app/main.js
@@ -2,12 +2,16 @@ var apiURL = 'http://localhost/personal/dash/public/api/'
 
 Vue.component('dungeon-select', {
   // options
-  template: '<select id="dungeon_select" v-model="selected"><option disabled value="">Please select a dungeon</option><option v-for="dungeon in dungeon_list" v-bind:value="dungeon.dg_id"> {{ dungeon.dg_name }} </option></select>',
+  template: `
+  <select id="dungeon_select" v-model="selected">
+    <option disabled value="">Please select a dungeon</option>
+    <option v-for="dungeon in dungeon_list" v-bind:value="dungeon.dg_id"> {{ dungeon.dg_name }} </option>
+  </select>
+  `,
   data: function() {
     return {
       selected: '',
       dungeon_list: [],
-      dungeon: []
     }
   },
   created: function() {
@@ -15,7 +19,7 @@ Vue.component('dungeon-select', {
   },
   watch: {
     selected: function() {
-      this.fetchDungeonData(this.selected)
+      this.$emit('selected', this.selected)
     }
   },
   methods: {
@@ -27,31 +31,113 @@ Vue.component('dungeon-select', {
         self.dungeon_list = JSON.parse(xhr.responseText)
       }
       xhr.send()
-    },
+    }
+  }
+})
+
+/*
+<svg>
+    <g v-for="pt in pts">
+        <rect x="{{pt.x}}" y="{{pt.y}}" width="{{z}}" height="{{z}}"></rect>
+    </g>
+</svg>
+*/
+
+Vue.component('dungeon-map', {
+
+  template: `
+  <div class="dungeon-map">
+    <div class="row">
+      <!-- dynamic dungeon select dropdown -->
+      <dungeon-select v-on:selected="fetchDungeonData"></dungeon-select>
+      <!-- percentage display div -->
+      <div id="percentage_display">{{ percent }}%</div>
+    </div>
+    <div class="row">
+        <svg id="map_overlay" v-on:click="placeDot">
+            
+            <g v-for="path in path_list">
+                <line :x1="path.start_x" :y1="path.start_y" :x2="path.end_x" :y2="path.end_y" stroke-width="2" stroke="black"/>
+            </g>
+
+            <g v-for="mob in mob_list">
+                <circle :cx="mob.x" :cy="mob.y" r="6" :class="mob.type" :data-percent="mob.percent" v-on:click="connectDots"/>
+                  <title>{{ mob.name }} - {{ mob.percent }}%</title>
+                </circle>
+            </g>
+
+        </svg>
+        <div class="map">
+          <img src="img/the_arcway.jpg" :src="dungeon.dg_image_path"/>
+        </div>
+    </div>
+  </div>
+  `,
+  data: function() {
+    return {
+      dungeon: [],
+      mob_list: [],
+      path_list: [],
+      percent: 0
+    }
+  },
+  created: function() {
+
+  },
+  methods: {
     fetchDungeonData: function(dungeon_id) {
       var xhr = new XMLHttpRequest()
       var self = this
-      xhr.open('GET', 'http://localhost/personal/dash/public/api/get_dungeon_data/'+dungeon_id)
+      xhr.open('GET', 'http://localhost/personal/dash/public/api/get_dungeon_data/' + dungeon_id)
       xhr.onload = function() {
-        self.dungeon = JSON.parse(xhr.responseText)
-        console.log(self.dungeon)
+        self.dungeon = JSON.parse(xhr.responseText);
+        var mob_json = JSON.parse(self.dungeon.dg_trash_array);
+        self.mob_list = mob_json.mobs;
+        self.path_list = [];
+        self.percent = 0;
       }
       xhr.send()
+    },
+    placeDot: function(event) {
+      var mouseX = event.clientX - 20;
+      var mouseY = event.clientY - 70;
+      console.log('('+mouseX+','+mouseY+')'); 
+    },
+    connectDots: function(event) {
+      var clicked_x = event.currentTarget.getAttribute('cx');
+      var clicked_y = event.currentTarget.getAttribute('cy');
+      console.log(event.currentTarget.getAttribute('data-percent'));
+      this.percent += parseInt(event.currentTarget.getAttribute('data-percent'));
+      console.log(this.percent);
+      if (this.path_list.length > 0) {
+        var last_point = this.path_list[this.path_list.length - 1];
+        var new_path = {
+          "start_x": last_point.end_x,
+          "start_y": last_point.end_y,
+          "end_x": clicked_x,
+          "end_y": clicked_y
+        };
+        this.path_list.push(new_path);
+      } else {
+        var starting_point = this.mob_list[0];
+        var new_path = {
+          "start_x": starting_point.x,
+          "start_y": starting_point.y,
+          "end_x": clicked_x,
+          "end_y": clicked_y
+        };
+        this.path_list.push(new_path);
+      }
     }
   }
 })
 
 var app = new Vue({
   el: '#app',
-  data: {
-    name: 'Vue.js'
-  },
-  // define methods under the `methods` object
-  methods: {
-    placeDot: function(event) {
-      // `this` inside methods points to the Vue instance
-      console.log('(' + event.clientX + ',' + event.clientY + ')');
-      // `event` is the native DOM event
-    }
-  }
+  template: `
+    <div id="app">
+        <!-- interactive dungeon map -->
+        <dungeon-map></dungeon-map>
+    </div>
+  `
 })
